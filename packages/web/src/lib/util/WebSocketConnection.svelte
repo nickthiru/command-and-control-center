971ff1938572eee21f0c10154a76b47aee950cc4
Nick Thiru <nickthiru@gmail.com>
2024-03-04 01:38:33 +0800

    Tried moving the websocket connection code into an onMount() component lifecycle method.

--- packages/web/src/lib/util/WebSocketConnection.svelte
+++ packages/web/src/lib/util/WebSocketConnection.svelte
@@ -1,45 +1,52 @@
+<!--
+  the websocket connection gets closed on its own. how to reconnect when this happens?
+ -->
+
 <script>
-  
-  import { onDestroy } from "svelte";
+  import { onMount, onDestroy } from "svelte";
   import { CdkStackApiStackWebSocketStackCA37355C as ApiStackWebSocketStack } from "../../../../cdk/outputs.json";
 
   console.log("(+) Inside 'WebSocketConnection.svelte'");
 
   export let pubsub;
 
-  let webSocket = new WebSocket(ApiStackWebSocketStack.DevStageWebSocketApiEndpoint);
+  // let webSocket = new WebSocket(ApiStackWebSocketStack.DevStageWebSocketApiEndpoint);
+  let webSocket;
 
-  webSocket.addEventListener("open", () => {
-    console.log("WebSocket is connected");
-  });
+  onMount(() => {
+    webSocket = new WebSocket(ApiStackWebSocketStack.DevStageWebSocketApiEndpoint);
 
-  webSocket.addEventListener("error", (error) => {
-    console.log("WebSocket error: " + JSON.stringify(error, null, 2));
-  });
-
-  webSocket.addEventListener("message", (event) => {
-    let data = JSON.parse(event.data);
-
-    let eventTopic = data.event;
-
-    let eventData = {
-      deviceId: data.deviceId,
-      longitude: data.longitude,
-      latitude: data.latitude,
-    };
-    console.log("(+) eventTopic: " + eventTopic);
-    console.log("(+) eventData: " + JSON.stringify(eventData));
-
-    pubsub.queue(eventTopic, eventData);
-  });
-
-  webSocket.addEventListener("close", (event) => {
-    // webSocket.close();
-    console.log("WebSocket is closed: " + event.reason);
+    webSocket.addEventListener("open", () => {
+      console.log("WebSocket is connected");
+    });
+  
+    webSocket.addEventListener("error", (error) => {
+      console.log("WebSocket error: " + JSON.stringify(error, null, 2));
+    });
+  
+    webSocket.addEventListener("message", (event) => {
+      let data = JSON.parse(event.data);
+  
+      let eventTopic = data.event;
+  
+      let eventData = {
+        deviceId: data.deviceId,
+        longitude: data.longitude,
+        latitude: data.latitude,
+      };
+      console.log("(+) eventTopic: " + eventTopic);
+      console.log("(+) eventData: " + JSON.stringify(eventData));
+  
+      pubsub.queue(eventTopic, eventData);
+    });
+  
+    webSocket.addEventListener("close", (event) => {
+      // webSocket.close();
+      console.log("WebSocket is closed: " + event.reason);
+    });
   });
 
   onDestroy(() => {
     webSocket.close();
   });
-
 </script>
---
+++ packages/web/src/lib/util/WebSocketConnection1.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { onDestroy } from "svelte";
+  import { CdkStackApiStackWebSocketStackCA37355C as ApiStackWebSocketStack } from "../../../../cdk/outputs.json";
+
+  console.log("(+) Inside 'WebSocketConnection.svelte'");
+
+  export let pubsub;
+
+  let webSocket = new WebSocket(ApiStackWebSocketStack.DevStageWebSocketApiEndpoint);
+
+  webSocket.addEventListener("open", () => {
+    console.log("WebSocket is connected");
+  });
+
+  webSocket.addEventListener("error", (error) => {
+    console.log("WebSocket error: " + JSON.stringify(error, null, 2));
+  });
+
+  webSocket.addEventListener("message", (event) => {
+    let data = JSON.parse(event.data);
+
+    let eventTopic = data.event;
+
+    let eventData = {
+      deviceId: data.deviceId,
+      longitude: data.longitude,
+      latitude: data.latitude,
+    };
+    console.log("(+) eventTopic: " + eventTopic);
+    console.log("(+) eventData: " + JSON.stringify(eventData));
+
+    pubsub.queue(eventTopic, eventData);
+  });
+
+  webSocket.addEventListener("close", (event) => {
+    // webSocket.close();
+    console.log("WebSocket is closed: " + event.reason);
+  });
+
+  onDestroy(() => {
+    webSocket.close();
+  });
+</script>
